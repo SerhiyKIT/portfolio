bed68c8da3d8b84f85b131067c3321a13bedab08
SerhiyKIT <sergii.kotik@gmail.com>
2023-10-09 17:01:26 +0300

    Add baground

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-import './App.css';
+import './App.scss';
+import { Wrapper } from './wrapper/Wrapper';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-
+        <div className='App_screen'>
+        <Wrapper />
+        </div>
       </div>
     </Provider>
   );
